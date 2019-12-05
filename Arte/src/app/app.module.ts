e14f6e61191074ed8c57f758eadb6ad73efc188c
Gunjan Hardasmalani <hardasmalani.g@husky.neu.edu>
2019-12-05 15:44:32 -0500

    Create New Component - Posts

--- Arte/src/app/app.module.ts
+++ Arte/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsComponent } from './components/posts/posts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PostsComponent,
   ],
   imports: [
     BrowserModule,
