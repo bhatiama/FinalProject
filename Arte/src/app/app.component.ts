311e45fb387703fa72d1f344db817c66639964d2
Pranav_Mujumdar <mujumdar.p@husky.neu.edu>
2019-12-06 11:15:20 -0500

    Adding Bootstrap dependancy migrated header and posts component to bootstrap

--- Arte/src/app/app.component.ts
+++ Arte/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Arte';
+  showVar: boolean = false;
+  showVar1: boolean = true;
+  constructor() { }
+  ngOnInit() {
+  }
 }
--- Arte/src/app/components/header/header.component.ts
+++ Arte/src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() todoSelected = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit() {
