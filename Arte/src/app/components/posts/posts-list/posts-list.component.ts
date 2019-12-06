7e6b7a367778629b93d74c32433c08caeace5a1f
Pranav_Mujumdar <mujumdar.p@husky.neu.edu>
2019-12-06 12:59:59 -0500

    added the ngfor to render the posts only if added

--- Arte/src/app/components/posts/posts-list/posts-list.component.ts
+++ Arte/src/app/components/posts/posts-list/posts-list.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts-list.component.scss']
 })
 export class PostsListComponent implements OnInit {
-
+  posts = [
+    // {title: "1", content:"1"},
+    // {title: "2", content:"2"},
+    // {title: "3", content:"3"}
+  ]
   constructor() { }
 
   ngOnInit() {
