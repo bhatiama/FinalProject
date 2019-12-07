d58014fffa8d8c346ad386bd7f505291b8c56216
Pranav_Mujumdar <mujumdar.p@husky.neu.edu>
2019-12-06 22:46:16 -0500

    /posts GET route workingg now checking for angular communication

---
+++ ArteBackend/controllers/postController.js
@@ -0,0 +1,26 @@
+
+'use strict';
+/** 
+ * @var postService this variable takes us to the toDoService.js
+*/
+// const toDoService = require('../services/postService');
+
+/** 
+ * @exports list this function exports as .list can be accessed by postController.list in the toDoRoutes GET Route to for /posts
+ * Enables the Read Operation to get all the ToDos from the database
+ * @param request this variable saves the request to enable console logging
+ * @param response this variable helps us send the response  
+*/
+exports.list = function (request, response) {
+    const posts = [
+        {
+            id: "asdadasdsda",
+            title: "post from server",
+            content: "from server hope this works"
+        }
+    ];
+    response.json({
+        message: "from server",
+        posts: posts
+    })
+};
