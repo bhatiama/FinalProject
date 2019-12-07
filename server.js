929afb9e5401a00f068642be9eab6c0e471199c1
Pranav_Mujumdar <mujumdar.p@husky.neu.edu>
2019-12-06 22:10:20 -0500

    added code for setting up the server

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
-const http = require('http');
+const http = require('http'),
+     express = require('express'),
+     app = express();
+     mongoose    = require("mongoose"),
+     initApp = require('./ArteBackend/app'),
+     bodyParser = require('body-parser'),
+     port = process.env.PORT || 3000;
 
-const app = require('./ArteBackend/app');
-const port = process.env.PORT || 3000;
-
-app.set('port',);
+// app.set('port',);
 const server = http.createServer(app);
 
-server.listen(port);
+app.use(bodyParser.urlencoded(
+    {
+        extended: true
+    }
+));
+
+mongoose.connect("mongodb+srv://user:user@maindatabase-qpzwn.mongodb.net/tododb?retryWrites=true&w=majority",{
+  useUnifiedTopology:true,
+  useFindAndModify:false
+  }).then(() => {
+  console.log("connected to DB");
+}).catch(() => {
+  console.log("connection failed");
+})
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+initApp(app);
+
+app.listen(port, ()=>{
+    console.log("server Up on port ${port}");
+});
