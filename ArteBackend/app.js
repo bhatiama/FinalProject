5409f73134786e72113547635d932fe651d8ea8d
Pranav_Mujumdar <mujumdar.p@husky.neu.edu>
2019-12-06 22:11:11 -0500

    exporting empty app.js to create server

--- ArteBackend/app.js
+++ ArteBackend/app.js
@@ -1,16 +1,12 @@
-const express = require('express'),
-  bodyParser = require('body-parser'),
-  app = express(),
-  mongoose = require('mongoose');
-
-mongoose.connect("mongodb+srv://user:user@maindatabase-qpzwn.mongodb.net/tododb?retryWrites=true&w=majority",{
-  useUnifiedTopology:true,
-  useFindAndModify:false
-  }).then(() => {
-  console.log("connected to DB");
-}).catch(() => {
-  console.log("connection failed");
-})
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+'use strict';
+/** 
+ * @exports module for routing the request to the routes also exporting the same 
+*/
+module.exports = function(app){
+    // //initializing db model
+    // const toDoModel = require('./models/toDoSchema');
+    // //initializing routes
+    // const toDoRoutes = require('./routes/toDoRoutes');
+    // toDoRoutes(app);
+    console.log("here")
+}
