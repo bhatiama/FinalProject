a851cb1b6d3fe1b95a143b63b94838a11cf0dcd6
Pranav_Mujumdar <mujumdar.p@husky.neu.edu>
2019-12-06 22:18:03 -0500

    commenting our mongoose connect for now to set backend and rechecked the server

--- server.js
+++ server.js
@@ -15,23 +15,31 @@ app.use(bodyParser.urlencoded(
     }
 ));
 
-mongoose.connect("mongodb+srv://user:user@maindatabase-qpzwn.mongodb.net/tododb?retryWrites=true&w=majority",{
-  useUnifiedTopology:true,
-  useFindAndModify:false
-  }).then(() => {
-  console.log("connected to DB");
-}).catch(() => {
-  console.log("connection failed");
-})
+// mongoose.connect("mongodb+srv://user:user@maindatabase-qpzwn.mongodb.net/postsdb?retryWrites=true&w=majority",{
+//   useUnifiedTopology:true,
+//   useFindAndModify:false
+//   }).then(() => {
+//   console.log("connected to DB");
+// }).catch(() => {
+//   console.log("connection failed");
+// })
 app.use(bodyParser.json());
+app.use((req,res,next)=>{
+    console.log("working");
+    next();
+})
 
 app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+app.use((req, res, next)=>{
+    res.send("hello")
+})
 initApp(app);
 
 app.listen(port, ()=>{
-    console.log("server Up on port ${port}");
+    console.log(`server Up on port ${port}`);
 });
