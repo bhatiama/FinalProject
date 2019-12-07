fde5e0585d393767a830b1f8dfdace0c7cb67ccd
Pranav_Mujumdar <mujumdar.p@husky.neu.edu>
2019-12-06 21:48:32 -0500

    added code to connect to mongo, and backend boiler =plate

--- ArteBackend/app.js
+++ ArteBackend/app.js
@@ -1,5 +1,16 @@
-const express =require('express');
+const express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express(),
+  mongoose = require('mongoose');
 
-const app = express();
+mongoose.connect("mongodb+srv://user:user@maindatabase-qpzwn.mongodb.net/tododb?retryWrites=true&w=majority",{
+  useUnifiedTopology:true,
+  useFindAndModify:false
+  }).then(() => {
+  console.log("connected to DB");
+}).catch(() => {
+  console.log("connection failed");
+})
 
-app.use();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
