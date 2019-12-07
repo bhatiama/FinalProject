aaa1bcaf3edf29cd23ec86f605ff6af1e31d7d8e
Pranav_Mujumdar <mujumdar.p@husky.neu.edu>
2019-12-06 22:33:38 -0500

    added the root route in the routes directory

---
+++ ArteBackend/routes/postRoutes.js
@@ -0,0 +1,18 @@
+'use strict';
+/** 
+ * @exports anon exports the routes from the controller
+ * Routes defined right now are 
+ * 1. / root route
+ * 2. /todos GET to get/read all the existing posts from the db
+ * 3. /todos POST to add/create a POST and add it to the db
+ * 4. /todos/:postID GET to read a specific post by getting the postid in the url
+ * 5. /todos/:postID PUT to update a specific post
+ * 6. /todos/:postID DELETE to delete a specific post
+ * 
+*/
+module.exports = function(app){
+    
+    app.get('/', (req,res)=>{
+        res.send("slash route");
+    })
+}
