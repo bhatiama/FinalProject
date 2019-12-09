2257efaeed124bf781f2ae2152197b93b7632dcd
Mayur <bhatia.ma@husky.neu.edu>
2019-12-09 01:17:48 -0500

    modify folder structure of angular

---
+++ Arte/src/app/services/post.service.ts
@@ -0,0 +1,38 @@
+import { Post } from '../models/post.model';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from "rxjs";
+
+@Injectable({ providedIn: 'root' })
+export class PostService {
+
+    constructor(private http: HttpClient) { };
+    private posts: Post[] = [];
+    private postsUpdated = new Subject<Post[]>();
+    getPosts() {
+        // return [...this.posts]
+        this.http.get<{ posts: Post[] }>(`http://localhost:3000/posts`).subscribe((postData) => {
+            this.posts = postData.posts;
+            this.postsUpdated.next([...this.posts]);
+        });
+    }
+
+    getPostUpdateListener() {
+        return this.postsUpdated.asObservable();
+    }
+    addPost(title: string, content: string) {
+        const post: Post = { id: null, title: title, content: content };
+        this.http.post<{ message: string }>(`http://localhost:3000/posts`, post)
+            .subscribe((resData) => {
+                console.log(resData.message);
+                this.posts.push(post);
+                this.postsUpdated.next([...this.posts]);
+            })
+    }
+
+    deletePost(postId : string){
+        this.http.delete(`http://localhost:3000/posts/`+postId).subscribe(()=>{
+            console.log('Deleted!');
+        })
+    }
+}
