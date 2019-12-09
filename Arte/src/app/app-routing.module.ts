a0276fe72be57bcd374ed6b44c4032f2a7e271bb
Harshit <shukla.har@husky.neu.edu>
2019-12-09 03:56:05 -0500

    Post layout

--- Arte/src/app/app-routing.module.ts
+++ Arte/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostsListComponent } from './components/posts/posts-list/posts-list.component';
 
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'posts', component: PostsListComponent }
 ];
 
 @NgModule({
